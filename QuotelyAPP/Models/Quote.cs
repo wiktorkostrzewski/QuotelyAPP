fdf9e218c02d3fd58c91cf5d8227cc9300a63dc5
Wiktor <x00195672@mytudublin.ie>
2025-11-24 15:37:18 +0000

    Service & Models Created

---
+++ QuotelyAPP/Models/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace QuotelyAPP.Models
+{
+    public class Quote
+    {
+        public string? _id { get; set; }
+        public string? content { get; set; }
+        public string? author { get; set; }
+        public List<string>? tags { get; set; }
+    }
+}
---
+++ QuotelyAPP/Services/QuoteService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using QuotelyAPP.Models;
+
+namespace QuotelyAPP.Services
+{
+    public class QuoteService
+    {
+        private readonly HttpClient _http;
+
+        public QuoteService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<Quote?> GetRandomQuote()
+        {
+            return await _http.GetFromJsonAsync<Quote>("random");
+        }
+    }
+}
